de99ddac416d98b868b7e980be9055e474bba449
Felipe.Herrera <cfherrera.mhd@gmail.com>
2025-06-07 10:56:58 -0500

    Commit del 07/06/25

--- front/src/components/AppointmentCard/AppointmentCard.jsx
+++ front/src/components/AppointmentCard/AppointmentCard.jsx
@@ -1,21 +1,18 @@
 import styles from "./AppointmentCard.module.css"
 const AppointmentCard = ({ appointment }) => {
-    const { id, date, time, status, user} = appointment;
+    const { id, date, time, status} = appointment;
 
     return (
         <div className={styles.body}>
             <h3>Appointment Id:{id}</h3>
             <p>
-                <strong>Date:</strong> {date}
+                <strong>📅Date:</strong> {date}
             </p>
             <p>
-                <strong>Time:</strong> {time}
+                <strong>⌚Time:</strong> {time}
             </p>
             <p>
                 <strong>Status:</strong> {status}
-            </p>
-            <p>
-                <strong>User:</strong> {user.name}
             </p>
         </div>
     );
--- front/src/views/Appointments/Appointments.jsx
+++ front/src/views/Appointments/Appointments.jsx
@@ -1,18 +1,29 @@
 import styles from "./Appointments.module.css";
-import {useState} from "react";
-import {appointmentsData} from "../../helpers/myAppointments";
+import {useState, useEffect} from "react";
 import AppointmentCard from "../../components/AppointmentCard/AppointmentCard";
+import axios from "axios";
 
 const Appointments = () => {
-    const [appointments, setAppointments] = useState(appointmentsData);
+    const [appointments, setAppointments] = useState([]);
+
+const apiCall = async () => {
+    const response = await axios.get("http://localhost:8080/appointments");
+    setAppointments(response.data.data);
+    console.log(response.data.data);
+    
+};
+
+useEffect(() => {
+    apiCall();
+},[])
     
     return (
     <main className={styles.body}>
         <h2 className={styles.title}>All appointments</h2>
         <div className={styles.appointmentsContainer}>
             {appointments.map((a) =>(
-            <div>
-                <AppointmentCard key={a.id} appointment = {a} />
+            <div key={a.id}>
+                <AppointmentCard appointment = {a} />
             </div>
         ))}
         </div>
